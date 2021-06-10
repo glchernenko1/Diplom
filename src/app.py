347068ac8fa7fcbbe984b93f783a9c2de308b174
Nick Coolberg <nick_coolberg@mail.ru>
2021-06-10 05:30:01 +0300

    front init

--- src/app.py
+++ src/app.py
@@ -4,18 +4,21 @@ from src.view import api as api_blueprint
 
 from flask import Flask, Response, json
 
+import os
+
 
 def create_app(env_name):
-    app = Flask(__name__, root_path='')
+    app = Flask(
+        __name__,
+        static_url_path='',
+        static_folder='frontend/dist')
+
     app.config.from_object(app_config[env_name])
 
     app.register_blueprint(api_blueprint, url_prefix='/api')
 
     @app.route('/', methods=['GET'])
     def index():
-        return Response(json.dumps({
-            "success": True,
-            "data": "Index page"
-        }), status=200, content_type='application/json')
+        return app.send_static_file('index.html')
 
     return app
